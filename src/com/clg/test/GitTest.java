e834f6b2908350cd14f0ce3cd88b0a2950909f3f
clglyj <33420809+clglyj@users.noreply.github.com>
2023-08-07 16:30:58 +0800

    Update GitTest.java
    删除代码

--- src/com/clg/test/GitTest.java
+++ src/com/clg/test/GitTest.java
@@ -2,12 +2,6 @@ package com.clg.test;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("=======================");
-        System.out.println("=======================");
-        System.out.println("=======================");
-        System.out.println("=======================");
-        System.out.println("=======================");
-        System.out.println("=======================");
         System.out.println("=======================12323456");
         System.out.println("add by  hot-fix");
         System.out.println("update by master");
